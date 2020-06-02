eaad19dc4c2f86b7f5b2411133b040f17e2daea4
Luke O'Sullivan <osulluke@gmail.com>
2020-06-01 20:40:18 -0700

    Add face scraper.
    This commit modifies the face_detect.py to take the actual image of the face and save it to a folder for later processing; adds a shell script to loop over the files.

--- sample-programs-demos/python/FaceDetect-master/face_detect.py
+++ sample-programs-demos/python/FaceDetect-master/face_detect.py
@@ -5,7 +5,11 @@ import sys
 
 # Get user supplied values
 imagePath = sys.argv[1]
+print(imagePath)
+file_name = imagePath.split('/')[-1].split('.')[0]
+file_name += '-face.jpg'
 cascPath = "FaceDetect-master/haarcascade_frontalface_default.xml"
+print(file_name)
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -24,11 +28,23 @@ faces = faceCascade.detectMultiScale(
 )
 
 print("Found {0} faces!".format(len(faces)))
-
+startx = 0
+endx = 0
+starty = 0
+endy = 0
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
+    print("rect:",(x,y,w,h))
+    startx = x
+    starty = y
+    endx = x + w
+    endy = y + h
+    face = image[starty:endy, startx:endx]
+    cv2.imshow("Face Only", face)
+    destination = 'img/faces/scraped-faces/' + file_name
+    cv2.imwrite(destination, face)
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
-cv2.waitKey(1700) # Pause with image found for 2 seconds
+cv2.waitKey(1000) # Pause with image found for 1 second
 cv2.destroyAllWindows()
--- sample-programs-demos/python/scan-faces.sc
+++ sample-programs-demos/python/scan-faces.sc
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 FILES=./img/faces/*
-
+rm ./img/faces/scraped-faces/*.jpg
 for f in $FILES
 do
     python FaceDetect-master/face_detect.py $f
