b08d96634455db5d57befd201701d2daf897dbba
Luke O'Sullivan <osulluke@gmail.com>
2020-06-04 11:39:52 -0700

    Update scale-image-set.py
    This little sample program calculates the 'average' face, which is an intermediate step required on the way to calculating the difference of each face from the average.

--- sample-programs-demos/python/scale-image-set.py
+++ sample-programs-demos/python/scale-image-set.py
@@ -1,12 +1,26 @@
 from PIL import Image
 import os
+import numpy as np
 
 read_path = 'img/faces/scraped-faces/'
 save_path = 'img/faces/scaled-faces/'
 entries = os.listdir(read_path)
 print(entries)
 
+average = 0
+i = 0
 for f in entries:
     im = Image.open(read_path + f)
     im = im.resize((300,300))
     im.save(save_path + f)
+    pixel_values = np.array(im)
+    pixel_values = pixel_values.astype('int32') # we need a larger data type to store data
+    average += pixel_values
+    i += 1
+
+average = average / i
+average = np.round(average)
+average = average.astype('uint8') # convert back into datatype appropriate for images
+
+average_face = Image.fromarray(average)
+average_face.show()
