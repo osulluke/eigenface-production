1ab9af7bd7e868d8a63548d794bbad39894f5aed
Luke O'Sullivan <osulluke@gmail.com>
2020-06-12 18:28:20 -0700

    Update the GUI.
    Makes it call python3; small change to the call to get a sub-process running; updates the README to show how to install kivy.

--- GUI_design/Readme.md
+++ GUI_design/Readme.md
@@ -6,6 +6,14 @@ Kivy is a Python GUI building tool.
 
 Download and install Kivy: [Kivy Installation Page](https://kivy.org/#download)
 
+```
+pip install kivy
+
+OR
+
+pip install kivy[base] kivy_examples --pre --extra-index-url https://kivy.org/downloads/simple/
+```
+
 
 ## Usage
 
--- GUI_design/main.py
+++ GUI_design/main.py
@@ -26,7 +26,7 @@ class ScreenTwo(Screen):
 
 class ScreenOne(Screen):
     def selectFile(self, btn):
-        sp.run(['python', 'filechooser.py'])
+        sp.run(['python3', 'filechooser.py'])
         with open('filechooser_result','r') as f:
             path = f.read()
         os.remove('filechooser_result')
