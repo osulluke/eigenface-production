ef492cf5794efd8ba1df04855a94bf3833234738
Remee <36454978+remeeliz@users.noreply.github.com>
2020-06-23 18:31:30 -0400

    flask
    a

---
+++ Flask_UI/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from templates import *
+from database import *
+from media_player import *
+from webstreaming import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    url_get = request.args.get('content')
+    if url_get == 'more_info':
+        return get_page(url_get)
+    if url_get == 'view_data':
+        return get_page('data_page')
+    else:
+        test()
+        return get_page('')
+
+if __name__ == "__main__":
+    app.run(debug=True)
