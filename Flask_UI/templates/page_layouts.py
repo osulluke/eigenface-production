328893473459fe716de9fbaec2ad908d49da7e47
Remee <36454978+remeeliz@users.noreply.github.com>
2020-06-29 11:07:09 -0400

    add tool tip

--- Flask_UI/templates/page_layouts.py
+++ Flask_UI/templates/page_layouts.py
@@ -13,6 +13,7 @@ from flask import url_for
 import urllib.parse
 import os
 import sys
+from video import get_videos
 import pandas as pd
 
 def get_page(page_name):
@@ -67,6 +68,8 @@ def info_page():
                 text("This is a facial recognition media player. After selecting a media file the software will parse facial images and insert them into the database. ")
                 text("These images will then be compared to current images and highlighted if recognized. The images will also be used to continue to grow the facial ")
                 text("recognition data set.")
+                doc.asis('</br></br>')
+                text('At a high level, this project\'s purpose is to be able to compare the image of a face against a database of known faces for identification, utilizing the concept known as Eigenface. Specifically, the vision I have for the way this project will be implemented is to take a video stream and "scrape" faces from it, turn them into X x Y images, then into a vector (XY x 1), and then measure the Euclidean distance between that face and all others in the database. Novel vectors that are outside a certain distance from known faces/vectors (i.e. faces that are in the database) will be classified as not recognized, and vectors that are within the distance will be classified as a certain individual contained within the database. This step could likely be implemented using K-means.')
                 text("")
                 doc.asis('</div>')
 
@@ -112,28 +115,30 @@ def buttons():
     with tag('div', id='container'):
         with tag('div', id='photo-container'):
             with tag('form', id='menu'):
-                doc.asis('<label id="button1" for="test" > SELECT YOUR MEDIA FILE </label><br>')
+                doc.asis('<label id="button1" for="test"  class="tooltip"> SELECT YOUR MEDIA FILE <span class="tooltiptext">The page will allow you to select media files (formatted as .mp4) that have been provided from some source, currently, these are part of the repo itself, and are limited to short clips of the television show "The Office." It is not necessary for the user to provide a novel file.</span></label><br>')
                 doc.asis('<input type="file" id="test" accept="image/png, image/jpeg, video/mp4">')
             with tag('form', id='menu'):
                 doc.asis('<label id="checkboxlabel" for="mute" > MUTE COMMERCIALS </label>')
                 doc.asis('<input type="checkbox" id="mute" name="mute" value="Mute"></br></br>')
-                doc.asis('<button type="submit" id="button2" class="tooltip" value="view_data"> PLAY MEDIA <span class="tooltiptext">The page will allow you to select media files (formatted as .mp4) that have been provided from some source, currently, these are part of the repo itself, and are limited to short clips of the television show "The Office." It is not necessary for the user to provide a novel file.</span></button>')
+                doc.asis('<button type="submit" id="button2" value="view_data" class="tooltip"> PLAY MEDIA <span class="tooltiptext">This page will then allow you to play that file using our technique that will scrape images of faces detected in the video stream, and identify them as a known character (actor) in the stream. Prior to playing the stream, the user can select what they would like to have happen when known faces are identified in the stream (i.e. mute, change the channel, etc.).</br></br>Currently, this function is limited to finding faces in the stream; identifying them has not yet been implemented. You can see, however, that as the stream is played, the face that is detected in the stream is identified and marked by a green square.</span></button>')
                 doc.asis('<textarea name="content" id="hide" method="post">data_page</textarea>')
             with tag('form', id='menu'):
-                doc.asis('<button type="submit" id="button4" value="learn_data" class="tooltip"> LEARN FACE <span class="tooltiptext">This page will then allow you to play that file using our technique that will scrape images of faces detected in the video stream, and identify them as a known character (actor) in the stream. Prior to playing the stream, the user can select what they would like to have happen when known faces are identified in the stream (i.e. mute, change the channel, etc.).</br></br>Currently, this function is limited to finding faces in the stream; identifying them has not yet been implemented. You can see, however, that as the stream is played, the face that is detected in the stream is identified and marked by a green square.</span></button>')
+                doc.asis('<button type="submit" id="button4" value="learn_data" class="tooltip"> LEARN FACE <span class="tooltiptext">In terms of the capabilities of the program, it will be initially trained on an image set derived from main characters in the show "The Office." As the program runs, however, and more faces are discovered, they should allow the model that identifies faces to be updated with these new faces so they can be categorized appropriately (as new characters, or commercial actors). On this page</br></br>, this is simply a mechanism to directly provide the model itself a novel face for incorporation into the model. Ultimately this will be a function that operates automatically during stream playback.</span></button>')
                 doc.asis('<textarea name="content" id="hide" method="post">learn_face</textarea>')
             with tag('form', id='menu'):
-                doc.asis('<button type="submit" id="button5" value="database"> VIEW DATA </button>')
+                doc.asis('<button type="submit" id="button5" value="database" class="tooltip"> VIEW DATA <span class="tooltiptext">This provides a view that our data API is actually working with; it will eventually provide more visibility in order to ensure data is uniform (correctly dimensioned, colored greyscale, etc).</span></button>')
                 doc.asis('<textarea name="content" id="hide" method="post">database</textarea>')
             with tag('form', id='menu'):
-                doc.asis('<button type="submit" id="button3" value="more_info"> MORE INFO </button>')
+                doc.asis('<button type="submit" id="button3" value="more_info" > MORE INFO </button>')
                 doc.asis('<textarea name="content" id="hide" method="post">more_info</textarea>')
 
     return doc.getvalue()
 
 def view_data_page():    
     doc, tag, text, line = Doc().ttl()
-
+    videolist = get_videos.video_list()
+    for video in videolist:
+        print(video)
     with tag('div', id='photo-container'):
         doc.asis('<iframe width="1000" height="500" src="'+url_for('video_feed')+'" frameborder="0" allowfullscreen></iframe>')
     return doc.getvalue()
