a4dd1ec9d089d15c8734c41e11ceeacfe1ccda75
Remee <36454978+remeeliz@users.noreply.github.com>
2020-06-30 21:15:24 -0400

    Fix file upload, add video player

--- Flask_UI/db_functions/ohmypydb.py
+++ Flask_UI/db_functions/ohmypydb.py
@@ -4,55 +4,103 @@
 ## Author: RDinmore
 ## Date: 2020.06.27
 ## Purpose: database functions
-## Libs: pandas
+## Libs: mysql
 ## Path: db/ohmypydb.py
 ##################################################
 
+from mysql import connector
 import pandas as pd
 
+mydb = connector.connect(user='admin', password="ohmypycis4930",
+                                   host="ohmypy.cdd0llcf03hp.us-east-1.rds.amazonaws.com", db="metadata")
+
+def execute_query(query):
+    mydb = connector.connect(user='admin', password="ohmypycis4930", host="ohmypy.cdd0llcf03hp.us-east-1.rds.amazonaws.com")
+    mycursor = mydb.cursor()
+    results = mycursor.execute(query)
+    if results == None:
+        return mycursor.fetchall()
+    else:
+        return 0
+
+
+def create_database(database_name):
+    query = "CREATE DATABASE IF NOT EXISTS "+database_name
+    execute_query(query)
+
 def get_face_id(face_vector, name_id):
-    data = pd.read_csv("face_data.csv")
-    face_id = data['face_vector'] == face_vector
-    face_id = data[face_id]
+    sql_select_Query = "SELECT MIN(face_id), count FROM face_data WHERE face_vector = '" + face_vector + "'"
+    cursor = mydb.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
 
-    if face_id.shape[0] == 0:
-        max_id = data.loc[data['face_id'].idxmax()]
-        max_id = max_id['face_id'] + 1
-        row = [{'face_id': max_id, 'face_vector': face_vector, 'name_id': name_id}]
-        new_df = pd.DataFrame(row)
+    face_id = records[0][0]
+    if face_id != None:
+        new_count = records[0][1] + 1
+        sql_select_Query = "UPDATE face_data SET count = " + str(new_count) + " WHERE face_id = " + str(face_id) + ""
+        cursor = mydb.cursor()
+        cursor.execute(sql_select_Query)
+        mydb.commit()
+        cursor.close()
+        return face_id
+    else:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO face_data (face_vector, name_id, count) VALUES ('" + face_vector + "',"+str(name_id)+", 1)")
+        mydb.commit()
+        cursor.close()
+        return get_name_id(face_vector)
 
-        with open("face_data.csv", 'a') as f:
-            new_df.to_csv(f, header=True)
+def get_face_id(face_vector, name_id):
+    sql_select_Query = "SELECT MIN(face_id), count FROM face_data WHERE face_vector = '" + face_vector + "'"
+    cursor = mydb.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
 
-        return (max_id)
-
-    return face_id['face_id'].to_string(index=False)
+    face_id = records[0][0]
+    if face_id != None:
+        new_count = records[0][1] + 1
+        sql_select_Query = "UPDATE face_data SET count = " + str(new_count) + " WHERE face_id = " + str(face_id) + ""
+        cursor = mydb.cursor()
+        cursor.execute(sql_select_Query)
+        mydb.commit()
+        cursor.close()
+        return face_id
+    else:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO face_data (face_vector, name_id, count) VALUES ('" + face_vector + "',"+str(name_id)+", 1)")
+        mydb.commit()
+        cursor.close()
+        return get_name_id(face_vector)
 
 def get_name_id(name):
-    data = pd.read_csv("name_data.csv")
-    name_id = data[data['full_name'] == name]['name_id']
+    sql_select_Query = "SELECT MIN(name_id) FROM name_data WHERE full_name = '"+name+"'"
+    cursor = mydb.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
 
-    if name_id.shape[0] == 0:
-        max_id = data.loc[data['name_id'].idxmax()]
-        max_id = max_id['name_id'] + 1
-        row = [{'name_id':max_id,'full_name':name}]
-        new_df = pd.DataFrame(row)
-        with open("name_data.csv",'a') as f:
-            new_df.to_csv(f, header=False)
-
-        return (max_id)
-
-    return name_id.to_string(index=False)
-
+    name_id = records[0][0]
+    if name_id != None:
+        return name_id
+    else:
+        cursor = mydb.cursor()
+        if len(name) > 100:
+            name = input("Name Error")
+        cursor.execute("INSERT INTO name_data (full_name) VALUES ('" + name + "')")
+        mydb.commit()
+        cursor.close()
+        return get_name_id(name)
 
 def insert_face(face_vector, name):
-    name_id = get_name_id(name)
-    get_face_id(face_vector,name_id)
+    name_id = get_name_id(str(name))
+    face_id = get_face_id(str(face_vector), name_id)
+    return 1
 
 def get_data():
-    face_data = pd.read_csv("face_data.csv")
-    name_data = pd.read_csv("name_data.csv")
-    df = name_data.join(face_data.set_index('name_id'), lsuffix='_other', on='name_id')
-    #return_df = df['full_name','face_vector'].copy()
+    sql_select_Query = "select n.full_name, f.face_vector, f.count from name_data n join face_data f on f.name_id = n.name_id;"
 
-    return df[["full_name","face_vector"]]
+    cursor = mydb.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    df = pd.DataFrame(records)
+
+    return df
--- Flask_UI/templates/page_layouts.py
+++ Flask_UI/templates/page_layouts.py
@@ -44,16 +44,37 @@ def get_page(page_name):
 def data_page(datatable):
     doc, tag, text, line = Doc().ttl()
     doc.asis(header())
-    html = datatable.to_html()
-    doc.asis(html)
+    with tag('div', id='container'):
+        doc.asis('<span>This provides a view that our data API is actually working with; it will eventually provide more visibility in order to ensure data is uniform (correctly dimensioned, colored greyscale, etc).</span>')
+        html = datatable.to_html()
+        doc.asis(html)
     doc.asis(footer())
     return doc.getvalue()
 
-
-def video_page(videoname):
+def display_page(directory_in):
     doc, tag, text, line = Doc().ttl()
     doc.asis(header())
-    doc.asis(view_data_page(videoname))
+    display_image(directory_in)
+    doc.asis(footer())
+    return doc.getvalue()
+
+def display_image(directory_in):
+    doc, tag, text, line = Doc().ttl()
+    with tag('div', id='photo-container'):
+        doc.asis('<iframe width="1000" height="500" src="' + url_for('video_feed',
+                                                                     video_name=directory_in) + '" frameborder="0" allowfullscreen></iframe>')
+    return doc.getvalue()
+
+def feature_page():
+    doc, tag, text, line = Doc().ttl()
+    doc.asis(header())
+    with tag('div', id='container'):
+        with tag('div', id='photo-container'):
+            text("Feature under development, please press button below to view example")
+            with tag('form', id='menu'):
+                doc.asis('<button type="submit" id="button2" value="view_data" class="tooltip"> PLAY MEDIA <span class="tooltiptext">This page will then allow you to play that file using our technique that will scrape images of faces detected in the video stream, and identify them as a known character (actor) in the stream. Prior to playing the stream, the user can select what they would like to have happen when known faces are identified in the stream (i.e. mute, change the channel, etc.).</br></br>Currently, this function is limited to finding faces in the stream; identifying them has not yet been implemented. You can see, however, that as the stream is played, the face that is detected in the stream is identified and marked by a green square.</span></button>')
+                doc.asis('<textarea name="content" id="hide" method="post">choose_video</textarea>')
+
     doc.asis(footer())
     return doc.getvalue()
 
@@ -76,7 +97,6 @@ def eval_face(response, image_name, output_count):
     doc.asis(footer())
     return doc.getvalue()
 
-
 def info_page():
     doc, tag, text, line = Doc().ttl()
     with tag('div'):
@@ -94,7 +114,6 @@ def info_page():
 
     return doc.getvalue()
 
-
 def footer():
     doc, tag, text, line = Doc().ttl()
 
@@ -109,7 +128,6 @@ def footer():
                             "Developers: Remee A., Martin L., Luke O., Zihan S., Xinxin W.")
 
     return doc.getvalue()
-
 
 def header():
     doc, tag, text, line = Doc().ttl()
@@ -132,21 +150,19 @@ def header():
 
     return doc.getvalue()
 
-
 def buttons():
     doc, tag, text, line = Doc().ttl()
 
     with tag('div', id='container'):
         with tag('div', id='photo-container'):
-            with tag('form', id='menu'):
-                doc.asis('<button id="button1" type="submit" value="upload_files" class="tooltip"> SELECT YOUR MEDIA FILE <span class="tooltiptext">The page will allow you to select media files (formatted as .mp4) that have been provided from some source, currently, these are part of the repo itself, and are limited to short clips of the television show "The Office." It is not necessary for the user to provide a novel file.</span></button><br>')
-                doc.asis(
-                    '<textarea name="content" id="hide" method="post">upload_files</textarea>')
-                # doc.asis('<input type="file" id="test" accept="image/png, image/jpeg, video/mp4">')
+            doc.asis('<form method=post id="menu1"  action="/upload_file" enctype=multipart/form-data>')
+            doc.asis('<label id="button1" for="test"  class="tooltip"> SELECT YOUR MEDIA FILE <span class="tooltiptext">The page will allow you to select media files (formatted as .mp4) that have been provided from some source, currently, these are part of the repo itself, and are limited to short clips of the television show "The Office." It is not necessary for the user to provide a novel file.</span></label><br>')
+            doc.asis('<textarea name="content" id="hide" method="post">upload_file</textarea>')
+            doc.asis('<input type="file" onchange="form.submit()" name="file" id="test" accept="image/png, image/jpeg, video/mp4">')
+            doc.asis('</form>')
             with tag('form', id='menu'):
                 doc.asis('<button type="submit" id="button2" value="view_data" class="tooltip"> PLAY MEDIA <span class="tooltiptext">This page will then allow you to play that file using our technique that will scrape images of faces detected in the video stream, and identify them as a known character (actor) in the stream. Prior to playing the stream, the user can select what they would like to have happen when known faces are identified in the stream (i.e. mute, change the channel, etc.).</br></br>Currently, this function is limited to finding faces in the stream; identifying them has not yet been implemented. You can see, however, that as the stream is played, the face that is detected in the stream is identified and marked by a green square.</span></button>')
-                doc.asis(
-                    '<textarea name="content" id="hide" method="post">choose_video</textarea>')
+                doc.asis('<textarea name="content" id="hide" method="post">choose_video</textarea>')
             with tag('form', id='menu'):
                 doc.asis('<button type="submit" id="button4" value="learn_data" class="tooltip"> LEARN FACE <span class="tooltiptext">In terms of the capabilities of the program, it will be initially trained on an image set derived from main characters in the show "The Office." As the program runs, however, and more faces are discovered, they should allow the model that identifies faces to be updated with these new faces so they can be categorized appropriately (as new characters, or commercial actors). On this page</br></br>, this is simply a mechanism to directly provide the model itself a novel face for incorporation into the model. Ultimately this will be a function that operates automatically during stream playback.</span></button>')
                 doc.asis(
@@ -163,28 +179,12 @@ def buttons():
 
     return doc.getvalue()
 
-
-def upload_files():
-    doc, tag, text, line = Doc().ttl()
-    with tag('div'):
-        text("Upload new File")
-        doc.asis('<form method=post enctype=multipart/form-data>')
-        doc.asis(
-            '<label for="file-upload" class="custom-file-upload"><i class="fa fa-cloud-upload"></i>Choose File</label></br>')
-        doc.asis(
-            '<input type="file" name="file" id="file-upload" accept="image/*, video/*" required></br></br>')
-        doc.asis('</br></br>')
-        doc.asis(
-            '<button type="submit" id="eval_button" value="Upload"> Upload </button>')
-        doc.asis('</form>')
-    return doc.getvalue()
-
-
 def choose_video():
     doc, tag, text, line = Doc().ttl()
     videolist = get_videos.video_list()
     with tag('div', id='container'):
         with tag('div', id='photo-container'):
+            doc.asis('<span> This page will then allow you to play that file using our technique that will scrape images of faces detected in the video stream, and identify them as a known character (actor) in the stream.Prior to playing the stream, the user can select what they would like to have happen when known faces are identified in the stream (i.e.mute, change the channel, etc.). Currently, this function is limited to finding faces in the stream; identifying them has not yet been implemented.You can see, however, that as the stream is played, the face that is detected in the stream is identified and marked by a green square.</span></br></br>')
             with tag('form', id='menu'):
                 for video in videolist:
                     videoname = (video[7:])[:-4]
@@ -195,12 +195,25 @@ def choose_video():
                     '<textarea name="content" id="hide" method="post">data_page</textarea>')
     return doc.getvalue()
 
+def video_page(videoname):
+    doc, tag, text, line = Doc().ttl()
+    doc.asis(header())
+    doc.asis(view_data_page(videoname))
+    doc.asis(footer())
+    return doc.getvalue()
 
 def view_data_page(videoname):
     doc, tag, text, line = Doc().ttl()
+    doc, tag, text, line = Doc().ttl()
     with tag('div', id='photo-container'):
-        doc.asis('<iframe width="1000" height="500" src="'+url_for('video_feed',
-                                                                   video_name=videoname)+'" frameborder="0" allowfullscreen></iframe>')
+        video_url = 'https://ohmypy-summer2020.s3.amazonaws.com/videos/' + videoname + '.mp4'
+        doc.asis('<div style="background-color:white"></br></br>')
+        doc.asis('<input type="checkbox" for="cb1"><label for="cb1">Block Face</label><input type="checkbox" for="cb2"><label for="cb2">Block Commercials</label><input type="checkbox" for="cb3"><label for="cb3">Replace Face</label></br>')
+        doc.asis('<audio controls autoplay><source src="' + video_url + '"></audio></br>')
+        doc.asis('<span><strong>This is a mock controller. In the future release the video will be able to be controlled to adjust your media output.</strong></br></br></br>')
+        doc.asis('</div>')
+        with tag('div', id='photo-container'):
+            doc.asis('<iframe width="100%" height="100%" src="'+url_for('video_feed', video_name=videoname)+'" frameborder="0" allowfullscreen></iframe>')
     return doc.getvalue()
 
 
@@ -209,7 +222,8 @@ def learn_face():
     with tag('div', id='container'):
         with tag('div', id='photo-container'):
             with tag('div'):
-                text("Choose an image with a single face to train facial recognition")
+                doc.asis('<span>In terms of the capabilities of the program, it will be initially trained on an image set derived from main characters in the show "The Office." As the program runs, however, and more faces are discovered, they should allow the model that identifies faces to be updated with these new faces so they can be categorized appropriately (as new characters, or commercial actors).On this page, this is simply a mechanism to directly provide the model itself a novel face for incorporation into the model.Ultimately this will be a function that operates automatically during stream playback.</span></br></br>')
+                doc.asis("<strong>Choose an image with a single face to train facial recognition</strong>")
                 with tag('div', id='learn_face'):
                     doc.asis('<form method=post enctype=multipart/form-data>')
                     doc.asis(
