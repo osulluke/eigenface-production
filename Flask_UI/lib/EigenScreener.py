2749e87c7e7d9cc2e2c10db25d4f71943d37b099
Luke O'Sullivan <osulluke@gmail.com>
2020-07-05 23:30:02 -0700

    Update EigenScreener.
    Adds test code to the EigenScreener class and shows a possible use case for the program; tests the ability to run a video, capture screen frames, scrape face images, and save them to disk.

--- Flask_UI/lib/EigenScreener.py
+++ Flask_UI/lib/EigenScreener.py
@@ -1,3 +1,7 @@
+import cv2
+import sys
+import time
+
 from TV_Watcher import TV_Watcher
 from DataConnector import DataConnector
 from FaceProcessor import FaceProcessor
@@ -20,12 +24,35 @@ class EigenScreener:
     """
     
     def __init__(self):
-        vid_stream = ""
-        tv_watcher = TV_Watcher()
-        data_connection = DataConnector()
-        face_processor = FaceProcessor()
-        face_space = FaceSpace()
-        stream_controller = StreamController()
+        #self.vid_stream = cv2.VideoCapture(vid)
+        self.tv_watcher = TV_Watcher()
+        self.data_connection = DataConnector()
+        self.face_processor = FaceProcessor()
+        self.face_space = FaceSpace()
+        self.stream_controller = StreamController()
 
 if __name__ == "__main__":
     eigen_screener = EigenScreener()
+    
+    video_stream = cv2.VideoCapture('../video/FinerThingsClub.mp4')
+    while True:
+        ret, frame = eigen_screener.tv_watcher.captureScreen(video_stream)
+        if time.time() % 3 <= 1:
+            eigen_screener.tv_watcher.scanForFaces(frame)
+            print("# detected_faces = " + str(len(eigen_screener.tv_watcher.detected_faces)))
+
+        if cv2.waitKey(30) & 0xFF == ord('q'): # https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
+            break
+        else:
+            try:
+                cv2.imshow('frame', frame)
+            except:
+                pass
+
+    for im in eigen_screener.tv_watcher.detected_faces:
+        eigen_screener.data_connection.insertImage(_image=im, _id=None)
+        cv2.imshow('frame', im)
+        cv2.waitKey(30)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
