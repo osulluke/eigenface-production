313801529d70b2b6c4f0c2b7247524d56237beb5
unknown <mozosjean@gmail.com>
2020-07-23 18:45:39 -0400

    Logic for mute based on commercial

--- Flask_UI/lib/sub_process_test.py
+++ Flask_UI/lib/sub_process_test.py
@@ -5,6 +5,11 @@ import cv2
 from face_space import face_space
 from tv_watcher import tv_watcher
 from eigen_screener import eigen_screener
+from selenium import webdriver
+from selenium.webdriver.common.by import BY
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 i = 0
 
 eigen_screener = eigen_screener()
@@ -25,4 +30,30 @@ while True:
         
     eigen_screener.tv_watcher.detected_faces.clear()
     # Code to determine if commerical or not
+
     # Code to activate or deactivate the mute (Selenium call)
+    driver = webdriver.Chrome()
+    driver.set_script_timeout(15)
+
+    # Getting our html link
+    url = "http://link"
+    driver.get(url)
+
+    video = EC.visibility_of_element_loated(By.TAG_NAME, 'video')
+    # Ensuring that the commercial is initializing 
+    driver.execute_async_script(
+        var video = argument[0],
+            callback = arguments[arguments.length -1]
+        video.addEventListener('loadstart', listener)
+
+        function listener(){
+            callback()
+        }, video
+    )
+    # Getting commercial from Luke code that determine if commercial is true or not
+    if(commercial = "true"){
+        driver.execute_script("argument[0].muted = true;", video)
+    }
+    elif(commercial = "false"){
+        driver.execute_script("argument[0].muted = false;", video)
+    }
